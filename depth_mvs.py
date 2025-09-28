637520b8b8bea10fc7307b70420d394086c79c3c
nanaijoel <joel.nanai@bluewin.ch>
2025-09-29 01:42:29 +0200

    Most stable version for 120 frames of a_buddha.MOV demo.

--- depth_mvs.py
+++
@@ -1,3 +0,0 @@
-def run_depth_mvs(images_folder: str, sfm_points, out_folder: str, on_log=None, on_progress=None):
-    on_log and on_log("[depth] (stub) übersprungen")
-    on_progress and on_progress(97, "Depth (stub)")
